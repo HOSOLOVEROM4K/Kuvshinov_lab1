3834d3e0c6cd16fe58d524b8b2d3f297a97f40a0
HOSOLOVEROM4K <151087930+HOSOLOVEROM4K@users.noreply.github.com>
2024-09-25 11:19:17 +0300

    добавляем меню, а также запрашиваем у пользователя номер команды

---
+++ LAB1/LAB1/LAB1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+void menu() {
+    setlocale(LC_ALL, "");
+    cout << "1. Добавить трубу" << endl;
+    cout << "2. Добавить КС" << endl;
+    cout << "3. Просмотр всех объектов" << endl;
+    cout << "4. Редактировать трубу" << endl;
+    cout << "5. Редактировать КС" << endl;
+    cout << "6. Сохранить данные в файл" << endl;
+    cout << "7. Загрузить данные из файла" << endl;
+    cout << "0. Выход" << endl;
+}
+
+
+// Меню программы
+int main() {
+
+    int input_index;
+    do {
+        menu();
+        cout << "Выберете команду: ";
+        cin >> input_index;
+        cout << "Индекс вашей программы: " << input_index << endl;
+
+    } while (input_index != 0);
+}
+
